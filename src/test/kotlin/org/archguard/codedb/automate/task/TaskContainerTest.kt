a1cf6b3cd6813b52984ca7c3a688c3b7987b919b
Phodal Huang <h@phodal.com>
2022-11-28 09:26:31 +0800

    feat(workflow): init lost actions

--- src/test/kotlin/org/archguard/codedb/automate/task/TaskContainerTest.kt
+++ src/test/kotlin/org/archguard/codedb/automate/task/TaskContainerTest.kt
@@ -2,6 +2,7 @@ package org.archguard.codedb.automate.task
 
 import org.archguard.codedb.automate.builtin.LocTask
 import org.junit.jupiter.api.Test
+import kotlin.reflect.KClass
 
 internal class TaskContainerTest {
 
@@ -21,14 +22,13 @@ internal class TaskContainerTest {
                 }
             }
             task("TaskName2") {
-                input {
-                    "String"
-                }
-                output {
+                dependsOn("TaskName", "TaskName 1")
+                input = String::class
+                output = Integer::class
 
-                }
                 taskAction {
-
+                    val input = it as String
+                    println(input)
                 }
             }
             handler("sample") {
@@ -40,23 +40,34 @@ internal class TaskContainerTest {
     class TaskDeclaration {
         fun input(function: () -> Any) {}
         fun output(function: () -> Any) {}
-        fun taskAction(function: () -> Any) {}
+        fun taskAction(function: (input: Any) -> Any) {}
+        fun dependsOn(vararg taskName: String) {
+            println(taskName)
+        }
 
-        var input: ()  -> Unit = {}
-        var output: () -> Unit = {}
+        var input: KClass<String>? = null
+        var output: KClass<Integer>? = null
+
         var taskAction: () -> Unit = {}
     }
 
-    private fun handler(handleName: String, function: () -> Unit) {
-        println("handler: $handleName")
+    class WorkflowDeclaration {
+        fun task(taskName: String, function: TaskDeclaration.() -> Unit) {
+            val taskDeclaration = TaskDeclaration()
+            taskDeclaration.function()
+            println(taskDeclaration.input)
+            println(taskDeclaration.output)
+            println(taskDeclaration.taskAction)
+        }
+        fun handler(handlerName: String, function: () -> Unit) {}
     }
 
-    private fun task(taskName: String, function: TaskDeclaration.() -> Unit) {
-        println("task: $taskName")
+    private fun workflow(flowName: String, function: WorkflowDeclaration.() -> Unit) {
+        val workflowDeclaration = WorkflowDeclaration()
+        workflowDeclaration.function()
     }
+}
 
-    private fun workflow(flowName: String, function: () -> Unit) {
-        println("Workflow: $flowName")
-        function()
-    }
+private infix fun Unit.h2(function: () -> Unit) {
+    TODO("Not yet implemented")
 }
