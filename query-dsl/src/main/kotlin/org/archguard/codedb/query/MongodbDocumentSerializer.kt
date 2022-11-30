6f72a6151ad6266b903f74af10844c675dcdb23f
Phodal Huang <h@phodal.com>
2022-11-30 23:39:09 +0800

    docs: init serializer for future design

---
+++ query-dsl/src/main/kotlin/org/archguard/codedb/query/MongodbDocumentSerializer.kt
@@ -0,0 +1,10 @@
+package org.archguard.codedb.query
+
+import com.querydsl.core.types.Visitor
+
+
+// todo: add document serializer
+abstract class MongodbDocumentSerializer : Visitor<Any, Void> {
+    //
+}
+
