a941901405ee797dcd5da08ca7c9e920fb3ac046
Phodal Huang <h@phodal.com>
2022-11-30 20:50:15 +0800

    fix: fix content

--- query-dsl/src/test/kotlin/org/archguard/codedb/query/MongoQueryTest.kt
+++ query-dsl/src/test/kotlin/org/archguard/codedb/query/MongoQueryTest.kt
@@ -3,10 +3,13 @@ package org.archguard.codedb.query
 import chapi.domain.core.CodeDataStruct
 import com.mongodb.MongoClient
 import com.querydsl.core.types.PathMetadata
-import com.querydsl.core.types.dsl.ComparablePath
+import com.querydsl.core.types.PathMetadataFactory
 import com.querydsl.core.types.dsl.EntityPathBase
+import com.querydsl.core.types.dsl.PathInits
+import com.querydsl.core.types.dsl.StringPath
 import com.querydsl.mongodb.morphia.MorphiaQuery
 import org.bson.types.ObjectId
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mongodb.morphia.Morphia
 import org.mongodb.morphia.annotations.Entity
@@ -21,18 +24,31 @@ class CodeDocument(
     val path: String = "",
 
     // data_struct
-    val ds: CodeDataStruct = CodeDataStruct(),
+    val ds: CodeDataStruct? = null,
     val ds_package: String = "",
     val ds_node_name: String = "",
     val ds_file_path: String = "",
 )
 
 class QCodeDocument : EntityPathBase<CodeDocument> {
-    val id: ComparablePath<ObjectId> = createComparable("id", ObjectId::class.java)
+    val systemId: StringPath = createString("systemId")
+
+    constructor(variable: String?) : this(
+        CodeDocument::class.java,
+        PathMetadataFactory.forVariable(variable),
+        PathInits.DIRECT2
+    )
 
     constructor(type: Class<out CodeDocument>?, metadata: PathMetadata?) : super(type, metadata)
-}
 
+    constructor(type: Class<out CodeDocument>?, metadata: PathMetadata?, inits: PathInits) : super(
+        type,
+        metadata,
+        inits
+    ) {
+
+    }
+}
 
 internal class MongoQueryTest {
     @Test
@@ -46,7 +62,10 @@ internal class MongoQueryTest {
         val datastore = morphia.createDatastore(mongo, "codedb")
 
         val query: MorphiaQuery<CodeDocument> = MorphiaQuery(morphia, datastore, CodeDocument::class.java)
+        val qCodeDocument = QCodeDocument("document")
+
         val first: CodeDocument = query
+            .where(qCodeDocument.systemId.eq("test"))
             .fetchFirst()
 
         println(first)
