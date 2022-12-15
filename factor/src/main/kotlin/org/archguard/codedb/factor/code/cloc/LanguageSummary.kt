5698ed25226112a56e605f492cb8b6352d60f2af
Phodal Huang <h@phodal.com>
2022-12-15 10:12:39 +0800

    refactor: remove unused fields for summary of cloc

--- factor/src/main/kotlin/org/archguard/codedb/factor/code/cloc/LanguageSummary.kt
+++ factor/src/main/kotlin/org/archguard/codedb/factor/code/cloc/LanguageSummary.kt
@@ -11,14 +11,12 @@ class LanguageSummary(
     val id: String,
     var name: String = "",
     var bytes: Long = 0,
-    var codeBytes: Long = 0,
     var lines: Long = 0,
     var code: Long = 0,
     var comment: Long = 0,
     var blank: Long = 0,
     var complexity: Long = 0,
-    var count: Long = 0,
-    var weightedComplexity: Double = 0.0,
+    var count: Long = 0
 ) {
     constructor() : this("")
 }
