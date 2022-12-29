3e5c08e2bcf15ae73cdfac3bf9f80991a5a3dfcf
Phodal Huang <h@phodal.com>
2022-12-29 10:54:28 +0800

    build: init libraries

--- codedb-ui/src/App.tsx
+++ codedb-ui/src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Box(props: any) {
+  // This reference gives us direct access to the THREE.Mesh object
+  const ref = useRef()
+  // Hold state for hovered and clicked events
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  // @ts-ignore
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  // Return the view, these are regular Threejs elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]}/>
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
+    </mesh>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <ambientLight/>
+      <pointLight position={[10, 10, 10]}/>
+      <Box position={[-1.2, 0, 0]}/>
+      <Box position={[1.2, 0, 0]}/>
+    </Canvas>
   );
 }
 
