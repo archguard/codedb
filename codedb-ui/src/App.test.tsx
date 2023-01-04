e872a19ccf140dbe2f3cb1ef61e9706bd27d7e56
Phodal Huang <h@phodal.com>
2023-01-04 15:16:23 +0800

    refactor: rename components

--- codedb-ui/src/App.test.tsx
+++ codedb-ui/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactThreeTestRenderer from '@react-three/test-renderer'
-import { Box } from "./App";
+import { Box } from "./components/threejs/Box";
 
 test('mesh to have two children', async () => {
   const renderer = await ReactThreeTestRenderer.create(<Box />)
--- codedb-ui/src/App.tsx
+++ codedb-ui/src/App.tsx
@@ -1,30 +1,8 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Canvas, useFrame } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { Flex } from '@react-three/flex';
-
-export function Box(props: any) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // @ts-ignore
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]}/>
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
-    </mesh>
-  )
-}
+import { Box } from "./components/threejs/Box";
 
 function App() {
   return (
