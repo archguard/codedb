3348fc1481c4467cae96c2ba9a3c8b36f522ba7f
Phodal Huang <h@phodal.com>
2023-01-06 10:17:51 +0800

    feat: init chart data

---
+++ codedb-ui/src/chart/chart-data.ts
@@ -0,0 +1,7 @@
+/**
+ * Chart data is a wrapper around the data that is passed to the chart.
+ * Receive JSON data and convert to different formats, for example: Radar, Line, Bar, etc.
+ */
+export interface ChartData {
+  type: string;
+}
