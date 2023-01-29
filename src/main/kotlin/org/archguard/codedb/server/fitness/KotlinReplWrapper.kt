3d067e53f34b16ecc61fc6b3cbfd1224ad541378
Phodal Huang <h@phodal.com>
2023-01-29 11:35:24 +0800

    refactor: upgrade jupyter kotlin version

--- src/main/kotlin/org/archguard/codedb/server/fitness/KotlinReplWrapper.kt
+++ src/main/kotlin/org/archguard/codedb/server/fitness/KotlinReplWrapper.kt
@@ -1,14 +1,12 @@
 package org.archguard.codedb.server.fitness
 
 import org.jetbrains.kotlinx.jupyter.EvalRequestData
-import org.jetbrains.kotlinx.jupyter.KernelConfig
 import org.jetbrains.kotlinx.jupyter.ReplForJupyter
-import org.jetbrains.kotlinx.jupyter.ReplForJupyterImpl
-import org.jetbrains.kotlinx.jupyter.RuntimeKernelProperties
 import org.jetbrains.kotlinx.jupyter.api.Code
 import org.jetbrains.kotlinx.jupyter.libraries.EmptyResolutionInfoProvider
 import org.jetbrains.kotlinx.jupyter.libraries.LibraryResolver
-import org.jetbrains.kotlinx.jupyter.messaging.DisplayHandler
+import org.jetbrains.kotlinx.jupyter.messaging.NoOpDisplayHandler
+import org.jetbrains.kotlinx.jupyter.repl.creating.createRepl
 import org.slf4j.LoggerFactory
 import java.io.File
 import kotlin.script.experimental.jvm.util.KotlinJars
@@ -16,15 +14,6 @@ import kotlin.script.experimental.jvm.util.KotlinJars
 class KotlinReplWrapper {
     private val logger = LoggerFactory.getLogger(this.javaClass)
     private val repl: ReplForJupyter
-    private val replRuntimeProperties = RuntimeKernelProperties(
-        mapOf(
-            "version" to "0.11.0.89.dev1",
-            "currentBranch" to "stable-kotlin",
-            "currentSha" to "3c9c34dae3d4a334809d3bb078012b743b2bd618",
-            "librariesFormatVersion" to "2",
-            "jvmTargetForSnippets" to "11"
-        )
-    )
 
     init {
         this.repl = this.makeEmbeddedRepl()
@@ -49,28 +38,21 @@ class KotlinReplWrapper {
         embeddedClasspath = embeddedClasspath.distinctBy { it.name } as MutableList<File>
         logger.info("classpath: $embeddedClasspath")
 
-        val config = KernelConfig(
-            ports = listOf(8080),
-            transport = "tcp",
-            signatureScheme = "hmac1-sha256",
-            signatureKey = "",
-            scriptClasspath = embeddedClasspath,
-            homeDir = null,
+        return createRepl(
+            EmptyResolutionInfoProvider,
+            embeddedClasspath,
             libraryResolver = resolveArchGuardLibs(),
-            embedded = true,
-            resolutionInfoProvider = EmptyResolutionInfoProvider,
+            displayHandler = NoOpDisplayHandler,
+            isEmbedded = true
         )
-
-        return ReplForJupyterImpl(config, this.replRuntimeProperties)
     }
 
     fun eval(
         code: Code,
-        displayHandler: DisplayHandler? = null,
         jupyterId: Int = -1,
         storeHistory: Boolean = true,
     ) =
-        repl.eval(EvalRequestData(code, displayHandler, jupyterId, storeHistory))
+        repl.eval(EvalRequestData(code, jupyterId, storeHistory))
 
     private fun resolveArchGuardLibs(): LibraryResolver {
         val lib = "archguard" to """
--- src/test/kotlin/org/archguard/codedb/server/ReplDemoTest.kt
+++ src/test/kotlin/org/archguard/codedb/server/ReplDemoTest.kt
@@ -2,7 +2,6 @@ package org.archguard.codedb.server
 
 import org.archguard.codedb.server.fitness.KotlinReplWrapper
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class ReplDemoTest {
@@ -15,7 +14,6 @@ class ReplDemoTest {
     }
 
     @Test
-    @Disabled
     internal fun simple_eval() {
         compiler.eval("val x = 3")
     }
