b7d0736211e7ade23cfb29209a410ecfe31cc107
Phodal Huang <h@phodal.com>
2023-02-01 21:40:13 +0800

    feat: add basic scm model & api

---
+++ workflow-lib/fitness-engine/src/main/kotlin/org/archguard/codedb/fitness/scm/SourceCodeManagement.kt
@@ -0,0 +1,33 @@
+package org.archguard.codedb.fitness.scm
+
+abstract class SourceCodeManagement {
+    abstract fun getCommitHistory(): List<Commit>
+    abstract fun getBranches(): List<Branch>
+    abstract fun getTags(): List<Tag>
+
+    abstract fun getCloc(): Cloc
+}
+
+// todo: merge with analyser_git
+class Cloc {
+    val language: String = ""
+    val blank: Int = 0
+    val comment: Int = 0
+    val code: Int = 0
+}
+
+class Tag {
+    val name: String = ""
+    val date: String = ""
+}
+
+class Branch {
+    val name: String = ""
+    val date: String = ""
+}
+
+class Commit {
+    val author: String = ""
+    val date: String = ""
+    val message: String = ""
+}
