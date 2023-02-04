1b21f249b6537898483b96e92ec0acacbb381f53
Phodal Huang <h@phodal.com>
2023-02-04 21:00:47 +0800

    refactor: extract codedb application

---
+++ client/codedb-clui/src/main/kotlin/org/archguard/codedb/clui/CodeDBApplication.kt
@@ -0,0 +1,66 @@
+package org.archguard.codedb.clui
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.input.key.onPreviewKeyEvent
+import androidx.compose.ui.input.key.type
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+@Preview
+fun CodeDBApplication() {
+    MaterialTheme {
+        var consumedText by remember { mutableStateOf(0) }
+        var text by remember { mutableStateOf("") }
+        Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
+            TextField(
+                placeholder = { Text("Run a command") },
+                value = text,
+                onValueChange = { text = it },
+                modifier = Modifier.onPreviewKeyEvent {
+                    when {
+                        (it.isCtrlPressed && it.key == Key.Minus && it.type == KeyEventType.KeyUp) -> {
+                            consumedText -= text.length
+                            text = ""
+                            true
+                        }
+
+                        (it.isCtrlPressed && it.key == Key.Equals && it.type == KeyEventType.KeyUp) -> {
+                            consumedText += text.length
+                            text = ""
+                            true
+                        }
+
+                        else -> false
+                    }
+                }
+            )
+
+            Button(modifier = Modifier.align(Alignment.CenterHorizontally),
+                onClick = {
+                    // todo: run the command
+                }) {
+                Text("Reset")
+            }
+        }
+    }
+}
