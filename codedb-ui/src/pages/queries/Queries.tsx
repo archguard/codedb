fa92fd3641328e386b0332a01bfba41339447221
Phodal Huang <h@phodal.com>
2023-03-01 15:17:22 +0800

    feat: init for apache arrow

--- codedb-ui/src/pages/queries/Queries.tsx
+++ codedb-ui/src/pages/queries/Queries.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { tableFromArrays } from 'apache-arrow/table'
 
 export interface QueriesProps {}
 
 function Queries(props: QueriesProps) {
+  const LENGTH = 2000
+
+  const rainAmounts = Float32Array.from({ length: LENGTH }, () => Number((Math.random() * 20).toFixed(1)))
+
+  const rainDates = Array.from({ length: LENGTH }, (_, i) => new Date(Date.now() - 1000 * 60 * 60 * 24 * i))
+
+  const rainfall = tableFromArrays({
+    precipitation: rainAmounts,
+    date: rainDates
+  })
+
+  console.table(rainfall)
+
   return (
     <div>
       <h1>Queries</h1>
