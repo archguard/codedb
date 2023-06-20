63ea280200fe4348ad028a2cb0f637bd1739d392
Phodal Huang <h@phodal.com>
2023-06-20 16:53:10 +0800

    chore: release 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ allprojects {
     }
 
     group = "org.archguard.codedb"
-    version = "0.1.0"
+    version = "0.1.1"
 
     java.sourceCompatibility = JavaVersion.VERSION_11
     java.targetCompatibility = JavaVersion.VERSION_11
