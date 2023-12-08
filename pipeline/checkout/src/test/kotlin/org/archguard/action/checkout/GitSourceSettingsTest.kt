76cede1c464202067c4ae707f09c4092a0dabe80
Phodal Huang <h@phodal.com>
2023-12-08 17:03:00 +0800

    chore: update test project

--- pipeline/checkout/src/test/kotlin/org/archguard/action/checkout/GitSourceSettingsTest.kt
+++ pipeline/checkout/src/test/kotlin/org/archguard/action/checkout/GitSourceSettingsTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class GitSourceSettingsTest {
     @Test
     fun `test from args for branch`() {
-        val repository = "https://github.com/archguard/codedb"
+        val repository = "https://github.com/archguard/ddd-monolithic-code-sample"
         val branch = "master"
 
         val settings = GitSourceSettings(
