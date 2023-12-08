230372b663b81e39279392994770928975aa1cb7
Phodal Huang <h@phodal.com>
2023-12-08 15:49:55 +0800

    chore: update for testing in local

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ allprojects {
     }
 
     group = "org.archguard.codedb"
-    version = "0.1.2"
+    version = "0.1.3"
 
     java.sourceCompatibility = JavaVersion.VERSION_11
     java.targetCompatibility = JavaVersion.VERSION_11
